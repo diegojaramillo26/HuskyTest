5d5ab57438aaea2b19574fc7888315e9b1ab1aa7
Diego Jaramillo <diego.jaramillo@ias.com.co>
2025-09-01 12:18:20 -0500

    chore: analizers configuration
    add analizers and code style configuration

--- HuskyTest/Controllers/WeatherForecastController.cs
+++ HuskyTest/Controllers/WeatherForecastController.cs
@@ -1,33 +1,50 @@
-using Microsoft.AspNetCore.Mvc;
-
 namespace HuskyTest.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Defines the <see cref="WeatherForecastController" />.
+    /// </summary>
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        /// <summary>
+        /// Defines the Summaries.
+        /// </summary>
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+        ];
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        /// <summary>
+        /// Defines the logger.
+        /// </summary>
+        private readonly ILogger<WeatherForecastController> logger;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WeatherForecastController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger<see cref="ILogger{WeatherForecastController}"/>Hello.</param>
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
         {
-            _logger = logger;
+            this.logger = logger;
         }
 
+        /// <summary>
+        /// The Get.
+        /// </summary>
+        /// <returns>The <see cref="IEnumerable{WeatherForecast}"/>hello.</returns>
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            this.logger.LogInformation("Hola");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)],
+            }).ToArray();
         }
     }
 }
